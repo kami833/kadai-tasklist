f1f3c864f88da7794b500d5c1f23df3daad92fad
kame.mi <amenomori.minakanusi.kamim@gmail.com>
2020-06-05 11:05:30 +0000

    taskuser login

---
+++ db/migrate/20200604061539_add_content_totasklist.rb
@@ -0,0 +1,4 @@
+class AddContentTotasklist < ActiveRecord::Migration[5.2]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_01_120435) do
+ActiveRecord::Schema.define(version: 2020_06_04_061539) do
+
+  create_table "microposts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "content"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_microposts_on_user_id"
+  end
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
@@ -29,5 +37,6 @@ ActiveRecord::Schema.define(version: 2020_06_01_120435) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "microposts", "users"
   add_foreign_key "tasks", "users"
 end
